1320f114d6582e7126ac1e24c69a9a52d6fc1670
s0ul <s0ul@protonmail.com>
2018-08-02 15:29:30 +0700

    Deploy config

--- sibDevBlog/settings.py
+++ sibDevBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jv)#+bh=^=@i1aqojv&^163@vgu#(46tpcv3&(g87jir9fa_9g'
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True  # debug config
 # sibdevblog.herokuapp.com
-ALLOWED_HOSTS = ['localhost']      # deploy config
+ALLOWED_HOSTS = ['sibdevblog.herokuapp.com']      # deploy config
 DEBUG = False
 
 # Application definition
